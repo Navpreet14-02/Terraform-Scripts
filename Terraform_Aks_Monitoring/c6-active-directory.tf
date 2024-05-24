d5b5fb7cf549e7a64a86be963d10020be588bd04
Navpreet14-02 <79501562+Navpreet14-02@users.noreply.github.com>
2024-05-24 13:54:33 +0530

    Delete Terraform_Aks_Monitoring directory

--- Terraform_Aks_Monitoring/c6-active-directory.tf
+++
@@ -1,6 +0,0 @@
-resource "azuread_group" "aks_admins"{
-    display_name = "${azurerm_resource_group.aks-rg.name}-cluster-admins"
-    description = "Azure aks admins for aks cluster"
-    security_enabled = true
-    
-}
--- Terraform_Aks_Monitoring/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-ad_tenant_id = "75f58a10-afdb-426a-a4a8-3f2004e96831"
-resource_group_name="aks_RG"
-resource_group_location = "Central India"
